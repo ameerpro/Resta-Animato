6fb423aa3d12bf0cff6075a7429fe00e51aa1ec4
Ameer Hamza <aha1475@gmail.com>
2022-01-02 16:35:49 +0500

    Initial Commit

---
+++ Resta Animato/Resta_AnimatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Resta_AnimatoApp.swift
+//  Resta Animato
+//
+//  Created by Ameer Hamza on 02/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Resta_AnimatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
